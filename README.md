4849c9400dadff12dda31a818f1b5879b1bdd57c
Md. Abdul Alim <92217313+AbdulAlimRony@users.noreply.github.com>
2023-07-30 15:57:02 +0600

    some plugins

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## About the POSTCSS
 
-<ul>
+<ul align="justify">
 <li>PostCSS is a tool to transform css plugin into javascript. Plugin ecosystem, Helps improve the performance, support upcoming css features.</li>
 <li>Cutting Edge Feature- Future CSS use in Present. Processing Speed is best</li>
 <li>Let's consider a common scenario where we want to add vendor prefixes to CSS properties for cross-browser compatibility. In normal CSS, we need to manually add these prefixes, which can be time-consuming and prone to errors. In Sass, we can use mixins or libraries like Compass to handle prefixes, but it still involves additional setup and may require updates. With PostCSS, we can use a plugin like Autoprefixer, which automatically adds the necessary vendor prefixes based on the specified browser support. PostCSS has plugin based modular architecture.</li>
--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,35 @@
-//Install PostCSS using node package manager(npm) for javascript and nodejs
-//Create All Base.. Folders and style.css, tailwind.config.js file manually
-//Install Plugin and add it Here
-//In package.json, write script - what will happen if we run postcss:watch command
-// --watch flag ensures if any change in input, output file will be changed instantly
+ /*
+|--------------------------------------------------------------------------
+| POSTCSS Configure
+|--------------------------------------------------------------------------
+|
+| 1. Install PostCSS using node package manager(npm) for javascript and nodejs
+| 2. Create All Base.. Folders and style.css, tailwind.config.js file manually
+| 3. Install Plugin and add it Here
+| 4. In package.json, write script - what will happen if we run postcss:watch command
+*/
 
 module.exports = {
     plugins: [
+        //To import file from our architecture- base, components, utilities
         require('postcss-import'),
+        
+        //Using Future CSS
+        require('postcss-preset-env')({
+            stage: 1,
+        }),
+
+        /*Minify the CSS, Written in Nodejs
+        Combining gzip compression with minification leads to the best reduction in file size.
+        we can divide cssnano's responsibilities between many plugins, each performing a small optimisation.*/
+        require('cssnano'),
+
+        require('autoprefixer'),
+
+        require('precss'),
+
+        require('postcss-assets')({
+            loadPaths: ['imgs'],
+        }),
     ]
 }
